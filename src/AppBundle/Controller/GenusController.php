87f9059fe1e6b865fbeebe3568d7afd73a93a448
Pancho <pancho.angarev@gmail.com>
2017-09-26 09:42:56 +0300

    aqau_note react.js

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+    /**
+     * @Route("/genus/{name}")
+     *
+     */
+
+    public function showAction($name)
+    {
+        $funFact = 'Octopuses can change the color of their *body in just* three-tenths of a second!';
+
+        $funFact = $this->get('markdown.parser')
+            ->transform($funFact);
+
+        return $this->render("genus/show.html.twig", [
+            'name'      => $name,
+            'funFact'   => $funFact
+        ]);
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
+     * @Method("GET")
+     */
+
+    public function getNotesAction()
+    {
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+
+        $data = [
+          'notes' => $notes,
+        ];
+
+        return new JsonResponse($data);
+
+    }
+
+}
