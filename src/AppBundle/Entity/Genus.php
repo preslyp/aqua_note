36770cc8cf3099d092916406f46b75b47aff5310
Pancho <pancho.angarev@gmail.com>
2017-09-26 14:47:29 +0300

    Add genus table via doctrin

---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pancho
+ * Date: 9/26/17
+ * Time: 2:34 PM
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus")
+ */
+class Genus
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+}
